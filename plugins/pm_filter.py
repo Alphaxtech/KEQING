c64693c0b94c3407a36570a51fa7c1b07d601fe8
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2022-12-03 11:56:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,7 +64,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{🎬get_size(file.file_size)}] {📂file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=🎬f"[{get_size(file.file_size)}] 📂{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -641,7 +641,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{🎬get_size(file.file_size)}] {📂file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=🎬f"[{get_size(file.file_size)}] 📂{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
