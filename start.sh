029824dcf52a5d3d20dcbd2a16b082ef0a21bc05
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-09-24 21:05:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sabith3938/GAGA_BOT
+  git clone https://github.com/Sabith3938/GAGA_BOT /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /GAGA_BOT
+  git clone $UPSTREAM_REPO /LazyPrincess
 fi
-cd /GAGA_BOT
+cd /LazyPrincess
 pip3 install -U -r requirements.txt
 echo "Starting SMP'S PRINCESS...."
 python3 bot.py
