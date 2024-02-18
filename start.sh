7c1722256f108105f9c68bff95769d254203ae7e
MR.ONLINE <157662527+Alphaxtech@users.noreply.github.com>
2024-02-18 21:20:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 if [ -z $UPSTREAM_REPO ]
+
 then
+
   echo "Cloning main Repository"
+
   git clone https://github.com/Sabith3938/GAGA_BOT /GAGA_BOT
+
 else
+
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
   git clone $UPSTREAM_REPO /GAGA_BOT
+
 fi
+
 cd /GAGA_BOT
+
 pip3 install -U -r requirements.txt
+
 echo "Starting SMP'S PRINCESS...."
+
 python3 bot.py
